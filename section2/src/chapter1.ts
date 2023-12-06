809f6d6cdd49b91e10e9f0e7645de0e226f1454e
songha <poan1221@gmail.com>
2023-12-06 23:54:39 +0900

    [section02] 기본타입/ 원시타입/ 리터럴타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,33 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// num1 = 'hello'; 문자열 넣을 수 ❌
+// num1.toUpperCase(); 문자열 전용 메서드 사용 ❌
+
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello ${str1}`;
+
+// boolean
+let bool1 : boolean = true;
+let bool2 : boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined 타입
+let unde1: undefined = undefined;
+
+// 리터럴 타입 -> 단 하나의 값을 가짐
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
+let boolB: false = false;
